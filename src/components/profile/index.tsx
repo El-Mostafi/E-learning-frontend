fafb24735bd560e60259b01a58c5e2138bf7e65e
MOHAMED EL MOSTAFI <elmoustafimohammed@gmail.com>
2025-06-11 14:27:47 +0100

    refactor: split ProfileForm into smaller components (SideBar, SettingPanel, dashboards) and add services for fetching dashboard stats

--- src/components/profile/index.tsx
+++ src/components/profile/index.tsx
@@ -4,7 +4,8 @@ import Preloader from "../../common/Preloader";
 import ScrollTop from "../../common/ScrollTop";
 import FooterOne from "../../layouts/footers/FooterOne";
 import HeaderOne from "../../layouts/headers/HeaderOne";
-import ProfileForm from "./ProfileForm";
+import ProfileForm from "./Profile Form/index";
+// import ProfileForm from "./ProfileForm";
 
  
 
