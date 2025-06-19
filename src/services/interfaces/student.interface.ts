822a99a32aef92841d4c4795cad8086c9ec94a70
MOHAMED EL MOSTAFI <elmoustafimohammed@gmail.com>
2025-06-20 00:10:56 +0100

    add certificates Issued to student dashboard

--- src/services/interfaces/student.interface.ts
+++ src/services/interfaces/student.interface.ts
@@ -2,6 +2,7 @@ export interface StudentStats {
   totalTimeLearned: number;
   coursesCompleted: number;
   activeCourses: number;
+  certificatesIssued: number;
   monthlyProgress: {
     coursesEnrolled: number;
     coursesCompleted: number;
