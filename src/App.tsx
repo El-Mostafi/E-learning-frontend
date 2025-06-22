3cbd6f71da291479fc12c62990b6ebfd1ea98144
BH-Hamza <hamza.b@ablcorporation.com>
2025-06-22 12:04:47 +0100

    changed protected routes

--- src/App.tsx
+++ src/App.tsx
@@ -41,7 +41,6 @@ import Profile from "./components/profile";
 import MyCourses from "./components/my-cours";
 import HomeTwo from "./components/homes/home-2";
 
-
 const router = createBrowserRouter([
   { path: "/", element: <HomeOne /> },
   { path: "/sign-in", element: <SignIn /> },
@@ -51,20 +50,21 @@ const router = createBrowserRouter([
   { path: "/reset-password", element: <ResetPassword /> },
   { path: "/contact", element: <Contact /> },
   { path: "/instructor", element: <Instructor /> },
-  { path: "/instructor-details/:id", element: <InstructorDetails /> },
+  { path: "/instructor-details", element: <InstructorDetails /> },
   { path: "*", element: <NotFound /> },
   { path: "/home-2", element: <HomeTwo /> },
   { path: "/home-3", element: <HomeThree /> },
   { path: "/home-4", element: <HomeFour /> },
   { path: "/home-5", element: <HomeFive /> },
+  { path: "/courses", element: <Courses /> },
+  { path: "/courses-grid", element: <CoursesGrid /> },
+  { path: "/courses-details", element: <CoursesDetails /> },
+  { path: "/about", element: <About /> },
 
   // Protected Routes
   {
     element: <ProtectedRoutes />, // Wrap all protected routes
     children: [
-      { path: "/courses", element: <Courses /> },
-      { path: "/courses-grid", element: <CoursesGrid /> },
-      { path: "/courses-details", element: <CoursesDetails /> },
       { path: "/courses-details-2", element: <CoursesDetailsTwo /> },
       { path: "/event", element: <Event /> },
       { path: "/event-details", element: <EventDetails /> },
@@ -75,7 +75,7 @@ const router = createBrowserRouter([
       { path: "/shop-details", element: <ShopDetails /> },
       { path: "/shop-cart", element: <ShopCart /> },
       { path: "/checkout", element: <Checkout /> },
-      { path: "/about", element: <About /> },
+      
       { path: "/program", element: <Program /> },
       { path: "/program-details", element: <ProgramDetails /> },
       { path: "/news", element: <News /> },
