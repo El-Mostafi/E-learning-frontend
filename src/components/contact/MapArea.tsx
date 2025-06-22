bd3fa7f4a73b7bb05870914dc01c3cc5a80d142f
BH-Hamza <hamza.b@ablcorporation.com>
2025-06-22 12:04:09 +0100

    Enhanced and made contact dynamic

--- src/components/contact/MapArea.tsx
+++ src/components/contact/MapArea.tsx
@@ -8,7 +8,7 @@ const MapArea = () => {
             <div className="row">
                 <div className="col-xl-12">
                     <div className="map-area">
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6678.7619084840835!2d144.9618311901502!3d-37.81450084255415!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad642b4758afc1d%3A0x3119cc820fdfc62e!2sEnvato!5e0!3m2!1sen!2sbd!4v1641984054261!5m2!1sen!2sbd" style={{border: "0px"}} allowFullScreen loading="lazy"></iframe>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2765.1520269182934!2d-4.9944527250460045!3d33.99650417317807!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd9f8b9a6b9ab9e1%3A0x64ee3e19ad13f28b!2sNational%20School%20of%20Applied%20Sciences!5e1!3m2!1sen!2sma!4v1750519945848!5m2!1sen!2sma" style={{border: "0px"}} allowFullScreen loading="lazy"></iframe>
                     </div>
                 </div>
             </div>
--- src/components/contact/index.tsx
+++ src/components/contact/index.tsx
@@ -2,28 +2,29 @@ import BreadcrumbEvent from "../../common/breadcrumb/BreadcrumbEvent";
 import MarqueeOne from "../../common/MarqueeOne";
 import Preloader from "../../common/Preloader";
 import ScrollTop from "../../common/ScrollTop";
+import { useAuth } from "../../context/AuthContext";
 import FooterOne from "../../layouts/footers/FooterOne";
+import FooterTwo from "../../layouts/footers/FooterTwo";
 import HeaderOne from "../../layouts/headers/HeaderOne";
 import ContactArea from "./ContactArea";
 import ContactForm from "./ContactForm";
 import MapArea from "./MapArea";
 
- 
-
 const Contact = () => {
-	return (
-		<>
-		<Preloader />
-			<HeaderOne />
-			<BreadcrumbEvent title="Contact" subtitle="Contact" />
-			<ContactArea />
+  const { user } = useAuth();
+  return (
+    <>
+      <Preloader />
+      <HeaderOne />
+      <BreadcrumbEvent title="Contact" subtitle="Contact" />
+      <ContactArea />
       <MapArea />
       <ContactForm />
-			<MarqueeOne style_2={true} />
-			<FooterOne />
-			<ScrollTop />
-		</>
-	);
+      <MarqueeOne style_2={true} />
+      {user ? <FooterOne user={user} /> : <FooterTwo />}
+      <ScrollTop />
+    </>
+  );
 };
 
 export default Contact;
